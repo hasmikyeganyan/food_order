cb8ce788e899d713f326485fb590d53127c7ba62
Hasmik Yeganyan <hasmikyeganyann@gmail.com>
2021-10-30 21:18:28 +0400

    first commit

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,11 @@
+import MealsSummary from "./MealsSummary";
+import AvailableMeals from "./AvailableMeals.js";
+const Meals = () => {
+  return (
+    <>
+      <MealsSummary />
+      <AvailableMeals />
+    </>
+  );
+};
+export default Meals;
